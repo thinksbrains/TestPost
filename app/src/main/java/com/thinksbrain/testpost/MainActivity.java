93d3db7d9cb6b19d26fb6ce784b974c06c7e81e0
4M1LK4R <amilkar.dominguez@gmail.com>
2017-10-20 00:10:40 -0400

    vista trabajos_Creacion de Adapter y Card Basico

--- app/src/main/java/com/thinksbrain/testpost/MainActivity.java
+++ app/src/main/java/com/thinksbrain/testpost/MainActivity.java
@@ -3,12 +3,51 @@ package com.thinksbrain.testpost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.thinksbrain.testpost.Objects.Trabajos;
+import com.thinksbrain.testpost.Utils.Variables;
+
+//Importaciones Necesarias
+import java.util.ArrayList;
+
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.thinksbrain.testpost.UI.TrabajoAdapter;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private TrabajoAdapter mAdapter;
+
+    public  Trabajos[] vector_trabajos = Variables.trabajos;
+    public ArrayList<Trabajos> list_trabajos = new ArrayList<>();
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //esta mal tu cambio
+
+        //Recorriendo vector para cargarlo en la Lista
+        for (int i=0; i<vector_trabajos.length; i++)
+        {
+            list_trabajos.add(vector_trabajos[i]);
+        }
+        //Asociando el Recycle View Trabajos
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycle_view_trabajos);
+        mRecyclerView.setHasFixedSize(true);
+
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new TrabajoAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setDataSet(list_trabajos);
     }
 }
